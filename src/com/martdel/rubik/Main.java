00a5dc86f350102a5c9d6379526e9cde66e61716
Martin Delebecque <martin-delebecque@outlook.fr>
2019-02-18 01:41:04 +0100

    Control of user input added : counter of color added to check that each color is present 8 times (a safety)

--- src/com/martdel/rubik/Main.java
+++ src/com/martdel/rubik/Main.java
@@ -57,39 +57,54 @@ public class Main {
 	}
 	
 	public static void askColor() {
-		System.out.println("Rentrer les couleurs des étiquettes du cube");
-		System.out.println("w pour white, r pour red, b pour blue, etc...");
-		System.out.println("Remplicez les informations pour chaque phrase une par une :");
-		for(Color faceColor : Color.values()) {
-			String faceOrientation = faceColor.getFaceOrientation();
-			String faceName = faceColor.getFrench();
-			Map<Integer, Color> array = new HashMap<>();
-			System.out.println("------------------------------------");
-			System.out.println("Face " + faceName + " : ");
-			System.out.println("Orientez la face " + faceName + " vers la face " + faceOrientation);
-			System.out.println("(face " + faceName + " face à vous et face " + faceOrientation + " derrière)");
-			array.put(1, input("Coin en haut à gauche :"));
-			array.put(2, input("Arête en haut au centre :"));
-			array.put(3, input("Coin en haut à droite :"));
-			array.put(4, input("Arête au milieu à gauche :"));
-			array.put(5, input("Arête au milieu à droite :"));
-			array.put(6, input("Coin en bas à gauche :"));
-			array.put(7, input("Arrête en bas au centre :"));
-			array.put(8, input("Coin en bas à droite :"));
-			Face face = new Face(faceColor, array.get(1), array.get(2), array.get(3), array.get(4), array.get(5), array.get(6), array.get(7), array.get(8));
-			addFaceInCube(faceName, face);
-		}
-	}
-
-	private static void addFaceInCube(String faceName, Face face) {
-		switch(faceName) {
-		case "blanche" : cube.setWhiteFace(face); break;
-		case "bleue" : cube.setBlueFace(face); break;
-		case "rouge" : cube.setRedFace(face); break;
-		case "verte" : cube.setGreenFace(face); break;
-		case "orange" : cube.setOrangeFace(face); break;
-		case "jaune" : cube.setYellowFace(face); break;
-		}
+		Integer whiteCounter = 0;
+		Integer blueCounter = 0;
+		Integer redCounter = 0;
+		Integer greenCounter = 0;
+		Integer orangeCounter = 0;
+		Integer yellowCounter = 0;
+		do {
+			whiteCounter = 0;
+			blueCounter = 0;
+			redCounter = 0;
+			greenCounter = 0;
+			orangeCounter = 0;
+			yellowCounter = 0;
+			System.out.println("Rentrer les couleurs des étiquettes du cube");
+			System.out.println("w pour white, r pour red, b pour blue, etc...");
+			System.out.println("Remplicez les informations pour chaque phrase une par une :");
+			System.out.println("! Si le programme se relance cela signifie que les données remplie ne sont pas valides !");
+			for(Color faceColor : Color.values()) {
+				String faceOrientation = faceColor.getFaceOrientation();
+				String faceName = faceColor.getFrench();
+				Map<Integer, Color> array = new HashMap<>();
+				System.out.println("------------------------------------");
+				System.out.println("Face " + faceName + " : ");
+				System.out.println("Orientez la face " + faceName + " vers la face " + faceOrientation);
+				System.out.println("(face " + faceName + " face à vous et face " + faceOrientation + " derrière)");
+				array.put(1, input("Coin en haut à gauche :"));
+				array.put(2, input("Arête en haut au centre :"));
+				array.put(3, input("Coin en haut à droite :"));
+				array.put(4, input("Arête au milieu à gauche :"));
+				array.put(5, input("Arête au milieu à droite :"));
+				array.put(6, input("Coin en bas à gauche :"));
+				array.put(7, input("Arrête en bas au centre :"));
+				array.put(8, input("Coin en bas à droite :"));
+				Face face = new Face(faceColor, array.get(1), array.get(2), array.get(3), array.get(4), array.get(5), array.get(6), array.get(7), array.get(8));
+				cube.setAttribut(faceColor, face);
+				// Safety :
+				for(int i = 1; i <= array.size(); i++) {
+					switch(array.get(i)) {
+					case WHITE: whiteCounter++; break;
+					case BLUE: blueCounter++; break;
+					case RED: redCounter++; break;
+					case GREEN: greenCounter++; break;
+					case ORANGE: orangeCounter++; break;
+					case YELLOW: yellowCounter++; break;
+					}
+				}
+			}
+		} while(whiteCounter != 8 || blueCounter != 8 || redCounter != 8 || greenCounter != 8 || orangeCounter != 8 || yellowCounter != 8);
 	}
 
 }
