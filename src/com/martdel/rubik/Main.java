1a79386ce11c6eea41bacc96584cd3b86e3e415b
root <root@localhost.localdomain>
2019-04-25 23:15:06 +0200

    Check if the code is compact (no) and do a test to check if the code is right (yes) : add a init function that init a cube solved to test the 'sexy move'

--- src/com/martdel/rubik/Main.java
+++ src/com/martdel/rubik/Main.java
@@ -15,7 +15,6 @@ public class Main {
 	public static void main(String[] args) {
 		cube = new Cube();
 		init();
-		// askColor();
 		firstCross();
 	}
 	
@@ -99,6 +98,28 @@ public class Main {
 		return bestCross;
 	}
 
+	// function to init the cube attribut without ask the colors to the user (cube solved)
+	public static void initSolved() {
+		Color w = Color.WHITE;
+		Color b = Color.BLUE;
+		Color r = Color.RED;
+		Color g = Color.GREEN;
+		Color o = Color.ORANGE;
+		Color y = Color.YELLOW;
+		Face whiteFace = new Face(w, w, w, w, w, w, w, w, w);
+		cube.setAttribut(w, whiteFace);
+		Face blueFace = new Face(b, b, b, b, b, b, b, b, b);
+		cube.setAttribut(b, blueFace);
+		Face redFace = new Face(r, r, r, r, r, r, r, r, r);
+		cube.setAttribut(r, redFace);
+		Face greenFace = new Face(g, g, g, g, g, g, g, g, g);
+		cube.setAttribut(g, greenFace);
+		Face orangeFace = new Face(o, o, o, o, o, o, o, o, o);
+		cube.setAttribut(o, orangeFace);
+		Face yellowFace = new Face(y, y, y, y, y, y, y, y, y);
+		cube.setAttribut(y, yellowFace);
+	}
+
 	// function to init the cube attribut without ask the colors to the user
 	public static void init() {
 		Color w = Color.WHITE;
@@ -176,10 +197,10 @@ public class Main {
 			greenCounter = 0;
 			orangeCounter = 0;
 			yellowCounter = 0;
-			System.out.println("Rentrer les couleurs des étiquettes du cube");
+			System.out.println("Rentrer les couleurs des ï¿½tiquettes du cube");
 			System.out.println("w pour white, r pour red, b pour blue, etc...");
 			System.out.println("Remplicez les informations pour chaque phrase une par une :");
-			System.out.println("! Si le programme se relance cela signifie que les données remplie ne sont pas valides !");
+			System.out.println("! Si le programme se relance cela signifie que les donnï¿½es remplie ne sont pas valides !");
 			for(Color faceColor : Color.values()) {
 				String faceOrientation = faceColor.getTop();
 				String faceName = faceColor.getFrench();
@@ -187,15 +208,15 @@ public class Main {
 				System.out.println("------------------------------------");
 				System.out.println("Face " + faceName + " : ");
 				System.out.println("Orientez la face " + faceName + " vers la face " + faceOrientation);
-				System.out.println("(face " + faceName + " face à vous et face " + faceOrientation + " derrière)");
-				array.put(1, input("Coin en haut à gauche :"));
-				array.put(2, input("Arête en haut au centre :"));
-				array.put(3, input("Coin en haut à droite :"));
-				array.put(4, input("Arête au milieu à gauche :"));
-				array.put(5, input("Arête au milieu à droite :"));
-				array.put(6, input("Coin en bas à gauche :"));
-				array.put(7, input("Arrête en bas au centre :"));
-				array.put(8, input("Coin en bas à droite :"));
+				System.out.println("(face " + faceName + " face ï¿½ vous et face " + faceOrientation + " derriï¿½re)");
+				array.put(1, input("Coin en haut ï¿½ gauche :"));
+				array.put(2, input("Arï¿½te en haut au centre :"));
+				array.put(3, input("Coin en haut ï¿½ droite :"));
+				array.put(4, input("Arï¿½te au milieu ï¿½ gauche :"));
+				array.put(5, input("Arï¿½te au milieu ï¿½ droite :"));
+				array.put(6, input("Coin en bas ï¿½ gauche :"));
+				array.put(7, input("Arrï¿½te en bas au centre :"));
+				array.put(8, input("Coin en bas ï¿½ droite :"));
 				Face face = new Face(faceColor, array.get(1), array.get(2), array.get(3), array.get(4), array.get(5), array.get(6), array.get(7), array.get(8));
 				cube.setAttribut(faceColor, face);
 				// Safety :
